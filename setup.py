c3e3556f6e0e801b5cd334a487f31b1ba40eb378
Chris Lamb <chris@chris-lamb.co.uk>
2015-11-14 10:07:12 +0200

    Releasing version 1.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='django-template-tests',
 
     url="https://chris-lamb.co.uk/projects/django-template-tests",
-    version='0.0.1',
+    version='1.0.0',
     description="Performs some quick static analysis on your templates",
 
     author="Chris Lamb",
