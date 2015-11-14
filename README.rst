e28a4e9d4c582fde2a8b919f04bcee380860bd42
Chris Lamb <chris@chris-lamb.co.uk>
2015-11-14 09:52:54 +0200

    Initial commit.

---
+++ README.rst
@@ -0,0 +1,15 @@
+django-template-tests
+=====================
+
+Performs some simple static analysis on your template files.
+
+Useful for catching obvious/preventable issues before they hit production.
+
+Installation
+------------
+
+ * Install to your PYTHONPATH, etc.
+
+ * Add ``template_tests`` to your ``INSTALLED_APPS``
+
+ * Run your tests as before.
---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-template-tests',
+
+    url="https://chris-lamb.co.uk/projects/django-template-tests",
+    version='0.0.1',
+    description="Performs some quick static analysis on your templates",
+
+    author="Chris Lamb",
+    author_email="chris@chris-lamb.co.uk",
+    license="BSD",
+
+    packages=find_packages(),
+    include_package_data=True,
+)
